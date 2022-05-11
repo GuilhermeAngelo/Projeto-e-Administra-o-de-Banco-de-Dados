13ee6163d214dae6f25462bbd93c1b7d329b2b9f
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2022-05-10 21:56:46 -0300

    tarefa01-q18 #1

---
+++ tarefas/t01/tarefa01-q18.sql
@@ -0,0 +1,15 @@
+-- Questão 18 
+
+create view maior_salario as
+select d.codigo ,max(f.salario) as salario
+from funcionario f left outer join departamento d
+on f.cod_depto = d.codigo
+group by d.codigo;
+
+create view resposta as 
+select distinct f.nome,f.salario
+from maior_salario s, funcionario f,departamento d
+where s.salario = f.salario
+order by f.salario;
+
+select nome from resposta;
