f943fc3fa2a2fdc861c7b5cea2c8fad554c14204
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2022-06-26 22:53:46 -0300

    5 letrac #7

---
+++ tarefas/t03/conection.js
@@ -0,0 +1,24 @@
+// npm install pg caso seja necessário instalar as depencias para usar os pacotes necessários
+// o pacote utilizado foi o pg
+
+const {Client} = require("pg")
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "postgres",
+    database: "AtividadesBD"
+})
+
+client.connect();
+
+// 5 - Use o programa de conexão feito na questão 3, e faça a execução dos seguintes comandos SQL
+// a - 
+
+
+// c-
+client.query("SELECT p.codigo as codigo_projeto,a.descricao FROM projeto p, atividade a WHERE a.codprojeto = p.codigo").
+then( results =>{
+    const resultado = results.rows;
+    console.table(resultado);
+}).finally( () => client.end())
---
+++ tarefas/t03/database_Config.js
@@ -0,0 +1,11 @@
+// npm install pg caso seja necessário instalar as depencias para usar os pacotes necessários
+// conexão relaziada usando funções do modulo pg pg  
+
+module.exports = {
+    dialect: postgres,
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "postgres",
+    database: "bd-rev"
+}
